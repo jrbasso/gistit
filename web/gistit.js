fabb70f0cf771312a7db729d98464348a15d1651
Juan Basso <jrbasso@gmail.com>
2012-06-30 23:56:41 -0400

    Under construction page

--- web/gistit.js
+++ web/gistit.js
@@ -2,8 +2,17 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.use(express.methodOverride());
+app.use(express.logger());
+app.use(express.bodyParser());
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
+app.use(express.errorHandler());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+  res.render('index');
 });
 
 var port = process.env.PORT || 80;
